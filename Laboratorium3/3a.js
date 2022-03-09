9f130e12b7dbba8c5e0daa3123fb0821646becf1
JuliaSidor811 <juliasidor811@gmail.com>
2022-03-09 18:22:10 +0100

    3b,3c

---
+++ Laboratorium3/3a.js
@@ -0,0 +1,4 @@
+/* Funkcja liczaca silnie zadanej liczby - dwie implementacje, jedna rekurencyjna, druga przy uzyciu iteracji.
+ Zaimplementuj funkcje rekurencyjna przy pomocy wyrazenia (function expression),
+ iteracyjna przy pomocy deklaracji (function declaration).
+ */
---
+++ Laboratorium3/3c.js
@@ -0,0 +1,34 @@
+//Funkcja sprawdza czy przekazany ciag jest palindromem
+
+function isPalindrom(sequence){
+    let result;
+    let temp;
+    if(typeof sequence === "string"){
+        temp = "";
+        for(let i = sequence.length-1; i>=0;i--){
+            temp += sequence[i]
+        }
+        result = temp === sequence;
+    }else{
+        temp = [];
+        for (let i = sequence.length-1; i>=0;i--){
+            temp.push(sequence[i])
+        }
+        let k = 0 // [1,2,3], [3,2,1]
+        while (k < sequence.length){
+            result = sequence[k] === temp[k]
+            if (result === false){
+                break
+            }
+            k +=1;
+
+        }
+    }
+
+    return result;
+}
+console.log(isPalindrom([6,5,4,4,5,6]))
+console.log(isPalindrom([843,23,21]))
+console.log(isPalindrom(["Ala","ma","kota",5]))
+console.log(isPalindrom("kajak"))
+console.log(isPalindrom("loteria a a"))
